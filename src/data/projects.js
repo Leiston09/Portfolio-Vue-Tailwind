269558c9c92a23563febb00956dc3f9ae438eaee
LeistonH <232432491+Leiston09@users.noreply.github.com>
2026-03-28 19:20:18 -0500

    Process: progress with the projects session

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,18 @@
-import imgProjectOne from "@/assets/img/gif/projectOne.gif";
-import imgProjectTwo from "@/assets/img/gif/projectTwo.gif";
-
 export const projects = [
   {
     id: 1,
     key: "portfolio",
-    image: imgProjectOne,
-    technologies: ["Vue 3", "Tailwind CSS", "JavaScript", "Vue Router", "i18n", "Pinia"],
+    image: new URL("@/assets/img/gif/projectOne.gif", import.meta.url).href,
+    technologies: [ "Vue 3","Tailwind CSS","JavaScript","Vue Router","i18n","Pinia",],
     github: "https://github.com/Leiston09/Portfolio-Vue-Tailwind.git",
-    demo: "https://ephemeral-cajeta-33ad99.netlify.app/"
+    demo: "https://ephemeral-cajeta-33ad99.netlify.app/",
   },
-
   {
     id: 2,
-    key:"pageAlex",
-    image: imgProjectTwo,
+    key: "pageAlex",
+    image: new URL("@/assets/img/gif/projectTwo.gif", import.meta.url).href,
     technologies: ["HTML", "CSS", "JavaScript", "PostgreSQL", "pgAdmin", "PHP"],
     github: "https://github.com/your-repo",
-    demo: "https://your-demo.com"
+    demo: "https://your-demo.com",
   },
-
 ];
