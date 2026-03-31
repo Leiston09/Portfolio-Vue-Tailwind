5489cef31dce6a73433d7ea1fae694c50a601541
LeistonH <232432491+Leiston09@users.noreply.github.com>
2026-03-31 14:58:41 -0500

    Process: Progress in the login session + design

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,18 @@
-
+import ProjectOne from "/img/gif/ProjectOne.gif";
 
 export const projects = [
   {
     id: 1,
     key: "portfolio",
-    image: "/img/gif/ProjectOne.gif",
-    technologies: [ "Vue 3","Tailwind CSS","JavaScript","Vue Router","i18n","Pinia",],
+    image: ProjectOne,
+    technologies: [
+      "Vue 3",
+      "Tailwind CSS",
+      "JavaScript",
+      "Vue Router",
+      "i18n",
+      "Pinia",
+    ],
     github: "https://github.com/Leiston09/Portfolio-Vue-Tailwind.git",
     demo: "https://ephemeral-cajeta-33ad99.netlify.app/",
   },
